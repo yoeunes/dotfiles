93450d53b341f29f86ab3449cfa92941c993e310
Younes Khoubza <younes.khoubza@gmail.com>
2019-11-06 11:42:03 +0100

    add gitignore global file

--- brew.sh
+++ brew.sh
@@ -121,11 +121,11 @@ brew bundle
 mkdir $HOME/projects
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+# rm -rf $HOME/.zshrc
+# ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+# ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Remove outdated versions from the cellar.
 brew cleanup
