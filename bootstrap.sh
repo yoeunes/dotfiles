b1af193313658b621e0286e3d6e74addf5238b6d
Younes Khoubza <younes.khoubza@gmail.com>
2019-11-11 14:45:13 +0100

    update bootstrap.sh file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE}")";
 
@@ -15,7 +15,7 @@ function doIt() {
 	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1"=="--force" -o "$1"=="-f" ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
